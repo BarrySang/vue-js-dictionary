45c49f3499b7eff8e5ede59a84e4ccb8f4fc7aef
BarrySang <barrysang8@gmail.com>
2024-06-25 01:38:05 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HeaderComponent title="Dictionary" />
+  <SectionComponent :wordSearched="wordSearched" :results="results" @transmitToApp="searchWord" />
+  <FooterComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import SectionComponent from './components/SectionComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponent,
+    SectionComponent,
+    FooterComponent,
+  },
+  
+  methods: {
+        async searchWord(word = '') {
+          this.wordSearched = true
+          console.log('word: ', word)
+          try {
+            const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
+            // console.log(response)
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            } else {
+                const data = await response.json()
+                console.log('data: ', data)
+                this.results = data
+            }
+          } catch(error) {
+            console.log('fetch error: ', error)
+            this.results = []
+          }
+        }
+    },
+  data() {
+    return {
+      results: [],
+      wordSearched: false
+    }
   }
 });
 </script>
---
+++ src/components/DefinitionComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="definition-container container">
+        <div class="definition-item">
+            <p class="data-title">Definition</p>
+            <p>{{ definition }}</p>
+        </div>
+        <div class="definition-item" v-if="example">
+            <p class="data-title">Example</p>
+            <p>{{ example }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DefinitionComponent',
+    props: {
+        definition: String,
+        example: String
+    }
+}
+</script>
+
+<style scoped>
+.definition-item {
+    margin: 3px;
+}
+
+.definition-container {
+    margin: 10px !important;
+    background-color: rgb(240, 234, 234) !important;
+    padding: 10px;
+    border-radius: 5px;
+    width: 95% !important;
+}
+
+.data-title {
+    color: rgb(34, 32, 32);
+}
+
+.definition-container p {
+    font-size: 18px;
+    display: block;
+    margin: 0;
+    
+}
+</style>
